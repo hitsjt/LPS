a5344a53401e2e49e0e2ec5557cb1ecd9d1b78b3
孙江涛 <SunJiangtao@th113116.ip.tsinghua.edu.cn>
2013-10-24 15:29:46 +0800

    Initial Commit

---
+++ LPS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  LPS
+//
+//  Created by 孙江涛 on 13-10-24.
+//  Copyright (c) 2013年 Bryan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
